490aa48b918eabd51178a5ae6c4bbbdf56cce021
Michael <uniquecyborg@gmail.com>
2022-07-04 13:34:44 -0700

    commited

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - Write a C program that prints exactly 'Programming is like building a multilingual puzzle'
- * Return 0 always (success)
+ * main - Write 'Programming is like building a multilingual puzzle'
+ * Returns 0 always (success)
  */
 int main(void)
 {
-	put("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
