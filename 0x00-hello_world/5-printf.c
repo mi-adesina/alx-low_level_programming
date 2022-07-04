30d8501814df9582cbb7d15d35d8157e404fb63e
Michael <uniquecyborg@gmail.com>
2022-07-04 13:42:45 -0700

    commited

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include <stdio.h>
+/*
+ * main -purt "with proper grammar, but the outcome is a piece of art,\n"
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
