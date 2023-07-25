c97aa553e90cbed881a24fd97a361de1aa52c51a
mi-adesina <swiftyrange@gmail.com>
2023-07-25 05:27:46 +0100

    All in

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "maim.h"
+
+/**
+ * _strlen - a function that returns the length of a string.
+ * @s: The required string
+ *
+ * Return: int(the lenght of the required string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0, j;
+
+	for (i = 0; s[j] != '\0'; j++)
+		i++;
+
+	return (i);
+}
