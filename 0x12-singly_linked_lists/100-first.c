250d71940d4b7399ae9f83bc53cef45138801db9
mi-adesina <swiftyrange@gmail.com>
2023-08-25 03:08:28 +0100

    5. The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * pre_main_message - Prints a string before the main function is executed.
+ *
+ * Return: nothing.
+ */
+void pre_main_message(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
