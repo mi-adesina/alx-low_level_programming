0e9b09cb7b02792aebff4c1688feaa97f7b72b5e
mi-adesina <swiftyrange@gmail.com>
2023-08-29 20:24:27 +0100

    a liitle correction

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,7 +13,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog = malloc(sizeof(dog_t));
 
-	if (new_dog == NULL)
+	if (new_dog == NULL || name == NULL || owner == NULL)
 		return (NULL);
 	new_dog->name = name;
 	new_dog->age = age;
